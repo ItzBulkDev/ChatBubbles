337e49dbc24923ac785790f33512da4c70c37f09
Philip Shilling <comedyman937@yahoo.com>
2014-06-18 08:36:56 -0400

    OCD...

--- ChatBubbles/src/TextManager.php
+++ ChatBubbles/src/TextManager.php
@@ -1,25 +1,31 @@
 <?php
+
 namespace ChatBubbles;
+
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Player;
 
 class TextManager extends PluginTask{
     private $c, $p;
+    
     public function onRun($tick){
         $this->remove();
         $this->revert();
         $this->c = true;
     }
+    
     public function remove(){
         $playername = $this->p->getName();
         $playername->setNameTag(" ");
         //This should just remove the text from the Name Tag
     }
+    
     public function revert(){
         $playername = $this->p->getName();
         $playername->setNameTag($player);
         //This should work
     }
+    
     public function createBubble($player, $message){
         $this->c = false;
         $this->p = $player;
@@ -29,4 +35,3 @@ class TextManager extends PluginTask{
         //I will have to try to seperate the messages into lines later
     }
 }
-?>
