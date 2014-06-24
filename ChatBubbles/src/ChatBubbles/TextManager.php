84cd0d525b3643bde5644dcad23ea3918b146c70
Philip Shilling <comedyman937@yahoo.com>
2014-06-24 10:37:40 -0400

    Minor Changes

--- ChatBubbles/src/ChatBubbles/TextManager.php
+++ ChatBubbles/src/ChatBubbles/TextManager.php
@@ -10,14 +10,14 @@ class TextManager extends PluginTask{
     public function onRun($tick){
         $this->revert();
     }
+    
     public function revert(){
         $this->p->setNameTag($this->p->getDisplayName());
     }
+    
     public function createBubble($player, $message){
         $this->p = $player;
         $this->m = $message;
-        $this->p->setNameTag($this->p->getDisplayName() . ":\n\n" . $this->m);
-        //This will not seperate the message into lines and may look ugly
-        //I will have to try to seperate the messages into lines later
+        $this->p->setNameTag("<" . $this->p->getDisplayName() . ">\n" . $this->m);
     }
 }
